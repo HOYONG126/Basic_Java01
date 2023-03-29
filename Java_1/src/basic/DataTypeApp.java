8600cfa898e440c96c96513224bfae3b414956b7
itwill <itwill@DESKTOP-AHDHN92>
2023-03-29 16:59:07 +0900

    3/29

--- Java_1/src/basic/DataTypeApp.java
+++ Java_1/src/basic/DataTypeApp.java
@@ -27,20 +27,18 @@ public class DataTypeApp {
 		System.out.print("boolean 형 : ");
 		boolean isChecked=true;
 		System.out.println(isChecked);
-		
 		System.out.println("=========================================");
-		
 		System.out.println(2+0+2+3+"은 토끼띠입니다.");  //의도에 반한 오류
 		System.out.println(""+2+0+2+3+"은 토끼띠입니다.");  //의도에 반한 오류를 수정 //NullString
 		System.out.println("올해는"+2+0+2+3+"년입니다.");
-		
 		System.out.println("=========================================");
 		int num1=100,num2=200;
 		System.out.println("연산결과="+num1+num2);  //의도에 반한 오류
 		System.out.println("연산결과="+(num1+num2));//의도에 반한 오류를 수정 //괄호사용
-		System.out.println("연산결과="+num1*num2);//사칙연산 순서적용 //그래도 곱하기여도 괄호사용하자
-		
-		
+		System.out.println("연산결과="+num1*num2);//사칙연산 순서적용 but 곱하기여도 괄호사용하자(실행속도 고려)
+		System.out.println("=========================================");
+		int korScore=88, engScore=90;
+		int sum=korScore+engScore;
+		System.out.println(sum);
 	}
-
 }
