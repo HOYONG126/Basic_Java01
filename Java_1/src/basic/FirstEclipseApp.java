ca91c7d9a7a02cb7f7db8ec35e415da4a457070a
itwill <itwill@DESKTOP-AHDHN92>
2023-03-29 12:31:31 +0900

    3/29 2023

---
+++ Java_1/src/basic/FirstEclipseApp.java
@@ -0,0 +1,16 @@
+//클래스가 작성된 패키지를 표현하기 위한 문장
+package basic;
+//이클립스 단축키
+//[ctrl]+[alt]+화살표 : 문장행복사
+//[alt]+화살표 : 문장이동
+public class FirstEclipseApp {
+	public static void main(String[] args) {
+		System.out.println("안녕하세요");
+		System.out.println("이클립스를 이용한 첫번째 프로그램입니다.");
+		System.out.println("재미있게 프로그램을 만들어 봐요.~!");
+	}
+}
+//행주석
+/*범위주석*/
+//깃허브 : 분산 버전 관리 툴인 깃 저장소 호스팅을 제공하는 웹 서비스
+//깃 git  : 여러명의 사용자들 간에 파일의 작업을 조율하기 위한 분산 버전 관리 시스템(프로그램)
