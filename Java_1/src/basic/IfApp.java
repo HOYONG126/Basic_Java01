ba369c0b8574703563de5c44a888c46b8378a628
itwill <itwill@DESKTOP-AHDHN92>
2023-03-31 12:50:30 +0900

    3

---
+++ Java_1/src/basic/IfApp.java
@@ -0,0 +1,41 @@
+package basic;
+
+public class IfApp {
+	public static void main(String[] args) {
+		//제어문(Control Statement)->프로그램흐름thread를 바꿔주는 명령
+		// => 선택문(if, switch), 반복문(for, while), 기타(break, continue, return)
+		/*
+		 * -if 조건식에 의해 명령 선택실행
+		 * if(조건식){명령; 명령; ... }
+		 *   		  조건식 통과후 명령이 하나만 작성된 경우에만 블럭{}기호 생략 가능
+		 * else
+		 */
+		int su=90;
+		if(su>=90) {
+			System.out.println("su = "+ su);
+		}
+		if(su>=90) System.out.println("su = "+ su);
+		System.out.println("================================================");
+		//if 조건식 틀릴때 출력방법
+		int score=80;
+		if(score>=60) System.out.println("합격입니다.");
+		else System.out.println("불합격입니다.");
+		int num=9;
+		System.out.println("================================================");
+		//변수값을 홀수와 짝수로 구분하여 출력
+		if(num%2==0) {
+			System.out.println("짝수");
+		}
+		else System.out.println("홀수");
+		System.out.println("================================================");
+		char mun='0';
+		//char mun2='ㅇ';
+		//문자변수에 저장된 문자값을 영문자와 비영문자로 구분하여 출력하는 프로그램 작성
+		if(mun>='A'&&mun<='Z'||mun>='a'&&mun<='z') {
+			System.out.println("영문자 입니다.");
+		}
+		else {
+			System.out.println("영문자가 아닙니다.");
+		}
+	}
+}
--- Java_1/src/example/OperatorExample.java
+++ Java_1/src/example/OperatorExample.java
@@ -7,9 +7,7 @@ public class OperatorExample {
 
 
 	public static void main(String[] args) {
-
 		//245678초를 일시분초 형식으로 변환하여 출력하세요.
-
 		int score=245678;
 		int day1=86400;
 		int time1=3600;
@@ -20,9 +18,6 @@ public class OperatorExample {
 		int second=(((score-day*day1)-time*time1)-minute*clock)%clock;
 		System.out.println(day+"일"+time+"시"+minute+"분"+second+"초");
 		System.out.println("===============================================");
-
-		
-		
 		//빼기 안쓰기  -- 문제에서 나온 변수를 변경하는 관점 
 		day=score/day1;
 		score%=day1;
@@ -32,18 +27,13 @@ public class OperatorExample {
 		score%=clock;
 		second=score;
 		System.out.println(day+"일"+time+"시"+minute+"분"+second+"초");
-		
-		
-		
 		//한대의 가격이 1억 5천원만인 비행기를 20대 구매할 경우 지불해야될 금액을 계산하여 출력하세요. 
 		//단, 15대 이상 구매할 경우 1대당 25%의 할인율을 적용하여 계산하세요.
-
 		int airplane = 150_000_000;
 		int number=20;
 		int disPriceNumber=15;
 		long allPrice=((number>=disPriceNumber)?(long)(airplane*0.75):airplane)*number;
 		System.out.println(allPrice+"원");
 		System.out.println("===============================================");
-		
 	}
 }
