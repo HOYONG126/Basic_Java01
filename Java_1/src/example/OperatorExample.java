e7428ae537dab41f193a6d0b90ca879c279f5a72
itwill <itwill@DESKTOP-AHDHN92>
2023-03-31 09:32:18 +0900

    3

--- Java_1/src/example/OperatorExample.java
+++ Java_1/src/example/OperatorExample.java
@@ -11,25 +11,14 @@ public class OperatorExample {
 		//245678초를 일시분초 형식으로 변환하여 출력하세요.
 
 		int score=245678;
-
 		int day1=86400;
-
 		int time1=3600;
-
 		int clock=60;
-
 		int day=score/day1;
-
 		int time=(score-day*day1)/time1;
-
 		int minute=((score-day*day1)-time*time1)/clock;
-
 		int second=(((score-day*day1)-time*time1)-minute*clock)%clock;
-
 		System.out.println(day+"일"+time+"시"+minute+"분"+second+"초");
-
-
-
 		System.out.println("===============================================");
 
 		//한대의 가격이 1억 5천원만인 비행기를 20대 구매할 경우 지불해야될 금액을 계산하여 출력하세요. 
@@ -37,19 +26,11 @@ public class OperatorExample {
 		//단, 15대 이상 구매할 경우 1대당 25%의 할인율을 적용하여 계산하세요.
 
 		int airplane = 150_000_000;
-
 		int number=20;
-
 		int disPriceNumber=15;
-
 		int discountPercent=75;
-
 		long allPrice=airplane*(long)disPriceNumber+(airplane*(number-disPriceNumber)*discountPercent/100);
-
 		System.out.println(allPrice+"원");
-
-
-
 		System.out.println("===============================================");
 
 	}
