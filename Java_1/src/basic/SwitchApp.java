64ace55428824531f2e0ff3ee53c43eae387c5af
itwill <itwill@DESKTOP-AHDHN92>
2023-03-31 16:27:11 +0900

    3

--- Java_1/src/basic/SwitchApp.java
+++ Java_1/src/basic/SwitchApp.java
@@ -6,14 +6,18 @@ public class SwitchApp {
 		/*형식) switch(비교대상)
 		 * 		case 값1: 명령; 명령; ... break;
 		 * 		case 값2: 명령; 명령; ... break;
-		 * 		case 값3: 명령; 명령; ... break;
+		 * 		case 값3: 명령; 명령; ... break;(생략가능)
 		 * 			...
 		 * 		default: 명령; 명령; ... (생략가능)
 		 * 
 		 *	 => 비교대상에는 변수 또는 연산식을 작성하여 비교하고자 하는 값 제공
-		 *   => 비교대상의 값을 case에 작성된 값과 차례대로 비교하여 같은 경우 해당 위치로부터 작성된 
-		 *   모든 명령 실행
+		 *		+double형 비교대상에 불가
 		 *   =>case 키워드로 제공되는 값은 리터럴 또는 상수만 작성가능
+		 *   	+변수불가
+		 *  // => case 키워드로 제공되는 값은 리터럴 또는 상수(Constant)만 작성 가능
+			// => case 키워드로 제공되는 값이 중복되어 작성될 경우 에러 발생
+			// => break 키워드를 만나면 switch 구문 강제 종료
+			// => case 키워드로 제공되는 값이 모든 다른 경우 default 키워드의 명령 실행 - 생략 가능
 		 *   
 		 */
 		int choice=4;
@@ -29,6 +33,24 @@ public class SwitchApp {
 		}
 		System.out.println("================================");
 		
+		int jumsu=84;
+		
+		String grade;
+		
+		switch(jumsu/10) {
+		case 10:
+		case 9:
+			grade="A"; break;
+		case 8:
+			grade="B"; break;
+		case 7:
+			grade="C"; break;
+		case 6:
+			grade="D"; break;
+		default:
+			grade="F"; break;
+		}
+		System.out.println("학점은 ="+grade);
 	}
 
 }
