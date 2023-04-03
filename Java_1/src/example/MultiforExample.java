6e92b3304583b516adb398b9f967c765a2db5e6b
HOYONG <HOYONG@DESKTOP-RV8K02D>
2023-04-03 21:15:59 +0900

    4/3

--- Java_1/src/example/MultiforExample.java
+++ Java_1/src/example/MultiforExample.java
@@ -41,38 +41,31 @@ public class MultiforExample {
 			for(int j=1; j<=2*i-1; j++) System.out.print("★");
 			System.out.println("");
 		}
-
 		System.out.println("===============================================================");
-		//★☆☆☆★
-		//☆★☆★☆
-		//☆☆★☆☆
-		//☆★☆★☆
-		//★☆☆☆★
-		int start=1;
-		int end=5;
-		int j=1;
-		int k=1;
-		for(int i=1; i<=5; i++) {
-			while(true) {
-				if(j==start) {
-					System.out.print("★");
-					j++;
+		loop: for(int i=1; i<=10; i++) {
+			for(int j=1; j<=i; j++) {
+				if(i%2==0) {
+					continue loop;
 				}
-				else if(j==end) {
-					System.out.print("★");
-					break;
-				}
-				else {
-					System.out.print("☆");
-					j++;
-				}
+			System.out.print("★");
 			}
-			end--;
-			start++;
 			System.out.println("");
-			j=1;
 		}
-		
+		System.out.println("===============================================================");
+		//★☆☆☆★ 1 5
+		//☆★☆★☆ 2 4 
+		//☆☆★☆☆ 3 3
+		//☆★☆★☆ 4 2 
+		//★☆☆☆★ 5 1
+		for(int i=1; i<=5; i++) {
+			for(int j=1; j<=5; j++) {
+				if(i==j||i+j==6) {
+					System.out.print("★");
+				}
+				else System.out.print("☆");
+			}
+			System.out.println("");
+		}
 		System.out.println("===============================================================");
 	}
 }
