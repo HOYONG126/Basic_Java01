a83bafcbfa71e1cc31db08b8738833451d894efc
itwill <itwill@DESKTOP-AHDHN92>
2023-04-04 11:14:28 +0900

    4

--- Java_1/src/example/MultiforExample.java
+++ Java_1/src/example/MultiforExample.java
@@ -41,6 +41,7 @@ public class MultiforExample {
 			for(int j=1; j<=2*i-1; j++) System.out.print("★");
 			System.out.println("");
 		}
+		
 		System.out.println("===============================================================");
 		loop: for(int i=1; i<=10; i++) {
 			for(int j=1; j<=i; j++) {
