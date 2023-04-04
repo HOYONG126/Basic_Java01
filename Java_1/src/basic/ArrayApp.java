fceda2a5f620b064d308ecda6a3f1e7e63b860c0
itwill <itwill@DESKTOP-AHDHN92>
2023-04-04 13:04:34 +0900

    4

---
+++ Java_1/src/basic/ArrayApp.java
@@ -0,0 +1,13 @@
+package basic;
+
+public class ArrayApp {
+	public static void main(String[] args) {
+		//Java 자료형 : 원시형과 참조형
+		//원시형 : 값을 표현하기 위한 자료형 - byte, short, int, long, char, float, double, boolean
+		//★참조형: 특정 대상을 표현하기 위한 자료형-배열(Array), 클래스, 인터페이스, 나열형
+		
+		
+		
+		
+}
+}
